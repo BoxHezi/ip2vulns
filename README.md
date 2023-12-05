4f2e8f517465ad1a473a46f44dd706f211af956d
boxhezi <547181293@qq.com>
2023-12-06 09:19:14 +1100

    update args, readme

--- README.md
+++ README.md
@@ -20,22 +20,22 @@ Using the following pip command to install: `pip install ip2vulns`
 # Usage
 
 ```text
-usage: ip2vulns [-h] [-inet INTERNETDB [INTERNETDB ...]] [-o OUT] [-s CVSS] [-d] [--downloaddb] [--ho] [-v]
+usage: ip2vulns [-h] [-i INTERNETDB [INTERNETDB ...]] [-o OUT] [-s CVSS] [-d] [--downloaddb] [--ho] [-v]
 
 IP 2 vulneribility tools
 
 options:
   -h, --help            show this help message and exit
-  -inet INTERNETDB [INTERNETDB ...], --internetdb INTERNETDB [INTERNETDB ...]
+  -i INTERNETDB [INTERNETDB ...], --internetdb INTERNETDB [INTERNETDB ...]
                         Query information from https://internetdb.shodan.io/
-                        support multiple ip and cidr, separate using space, e.g. -inet 8.8.8.8 51.83.59.99 192.168.0.0/24
+                        support multiple ip and cidr, separate using space, e.g. -i 8.8.8.8 51.83.59.99 192.168.0.0/24
                         if no database if specified, use ./databases/internetdb.db
+  -s CVSS, --cvss CVSS  Enable cvss score filter, required a number
+                        If 0 is given, targets found with no CVE information will be filtered out. And all CVEs will be checked.
+                        When 0 is given, the process can be slow if huge amount of CVEs are founded. Not Recommend.
   -o OUT, --out OUT     Define output file, default print to stdout
                         Available option: stdout (default), csv, json
                         Note: if -db flag is enabled, -out option will be disabled
-  -s CVSS, --cvss CVSS  Enable cvss score filter, required a number
-                        If 0 is given, targets found with no CVE information will be filtered out. And all CVEs will be checked.
-                        When 0 is given, the process can be slow if huge amount of CVEs are founded. Not Recommend to pass 0 in.
   --ho                  Output hostnames only for scan result.
                         This option DOES NOT apply to -d/--database option
   -v, --version         Print current version
--- ip2vulns/ip2vulns.py
+++ ip2vulns/ip2vulns.py
@@ -2,27 +2,22 @@ import argparse
 
 from . import version
 from . import utils
-from .Services import InternetDBService, CVEService
+from .Services import InternetDBService
 
 
 def init_argparse():
     arg = argparse.ArgumentParser(description="IP 2 vulneribility tools", formatter_class=argparse.RawTextHelpFormatter)
-    arg.add_argument("-inet", "--internetdb", help="Query information from https://internetdb.shodan.io/\n"
+    arg.add_argument("-i", "--internetdb", help="Query information from https://internetdb.shodan.io/\n"
                                                    "support multiple ip and cidr, separate using space, "
-                                                   "e.g. -inet 8.8.8.8 51.83.59.99 192.168.0.0/24\n"
+                                                   "e.g. -i 8.8.8.8 51.83.59.99 192.168.0.0/24\n"
                                                    "if no database if specified, use ./databases/internetdb.db",
                      nargs="+")
+    arg.add_argument("-s", "--cvss", help="Enable cvss score filter, required a number\n"
+                                        "If 0 is given, targets found with no CVE information will be filtered out. And all CVEs will be checked.\n"
+                                        "When 0 is given, the process can be slow if huge amount of CVEs are founded. Not Recommend to pass 0 in.")
     arg.add_argument("-o", "--out", help="Define output file, default print to stdout\n"
                                          "Available option: stdout (default), csv, json\n"
                                          "Note: if -db flag is enabled, -out option will be disabled")
-    arg.add_argument("-s", "--cvss", help="Enable cvss score filter, required a number\n"
-                                          "If 0 is given, targets found with no CVE information will be filtered out. And all CVEs will be checked.\n"
-                                          "When 0 is given, the process can be slow if huge amount of CVEs are founded. Not Recommend to pass 0 in.")
-    arg.add_argument("-d", "--database", help="Write result to database, using SQLite3 database\n"
-                                               "if no -o flag is provide, write data to internetdb.db in the same directory",
-                    action="store_true")
-    arg.add_argument("--downloaddb", help="download CAPEC and CWE database, csv file, store in ./databases directory",
-                     action="store_true")
     arg.add_argument("--ho", help="Output hostnames only for scan result.\n"
                      "This option DOES NOT apply to -d/--database option", action="store_true")
     arg.add_argument("-v", "--version", help="Print current version", action="store_true")
@@ -37,9 +32,6 @@ def main():
     if args.internetdb:  # type(internetdb) => list
         # InternetDBService.start(args.internetdb, args.out, args.database, args.cvss, args.ho)
         InternetDBService.start(args.internetdb, args.out, args.cvss, args.ho)
-
-    if args.downloaddb:  # download CAPEC and CWE database
-        CVEService.download_local_db()
 
     if args.version:
         print(version.__version__)
